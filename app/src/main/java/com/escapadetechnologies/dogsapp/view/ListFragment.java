b120dbb8a4a8f85da9317783f26e5ccd1ef221bf
user <g.brahmadatta@gmail.com>
2019-09-04 17:45:47 +0530

    Initial commit

---
+++ app/src/main/java/com/escapadetechnologies/dogsapp/view/ListFragment.java
@@ -0,0 +1,27 @@
+package com.escapadetechnologies.dogsapp.view;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.escapadetechnologies.dogsapp.R;
+
+
+public class ListFragment extends Fragment {
+
+    public ListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+}

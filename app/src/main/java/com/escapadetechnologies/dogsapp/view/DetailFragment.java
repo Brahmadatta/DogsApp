77054a25df4aff8aff5f7d84786590008552c958
user <g.brahmadatta@gmail.com>
2019-09-07 16:56:56 +0530

    card layout

--- app/src/main/java/com/escapadetechnologies/dogsapp/view/DetailFragment.java
+++ app/src/main/java/com/escapadetechnologies/dogsapp/view/DetailFragment.java
@@ -3,19 +3,30 @@ package com.escapadetechnologies.dogsapp.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.escapadetechnologies.dogsapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DetailFragment extends Fragment {
 
+
+    private int dogUuid;
 
     public DetailFragment() {
     }
@@ -24,7 +35,11 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        ButterKnife.bind(this,view);
+
+        return view;
     }
 
 }
